b91a504b37d56a16d1d673613072c8f586653919
SerafAC <seraf_ac@hotmail.com>
2026-03-14 12:05:36 +0100

    Start view unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     environment: 'node',
     include: ['tests/**/*.test.ts'],
@@ -9,6 +11,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/popup/scss/variables.scss";
+          @use "@/popup/scss/mixins.scss";
+        `,
+      },
     },
   },
 })
